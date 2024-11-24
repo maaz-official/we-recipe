159d5a29b973cda37292bfb461537de609f952ed
maaz-official <muhammadmaaz07@gmail.com>
2024-11-24 02:59:33 -0800

    cors added

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-// server.js
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors'; // Import cors
 import connectDB from './config/db.js';
 import recipeRoutes from './routes/recipeRoutes.js';
-import userRoutes from './routes/userRoutes.js'
+import userRoutes from './routes/userRoutes.js';
 
 dotenv.config();
 connectDB(); // Optional if you use a database
@@ -12,12 +12,12 @@ const app = express();
 const PORT = process.env.PORT || 4000;
 
 // Middleware
+app.use(cors()); // Enable CORS
 app.use(express.json());
 
 // Routes
 app.use('/recipes', recipeRoutes);
 app.use('/users', userRoutes);
-
 
 // Start the server
 app.listen(PORT, () => {

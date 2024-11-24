c2d1da766cf3bd7ccba86a898650fedd742a599c
maaz-official <muhammadmaaz07@gmail.com>
2024-11-24 03:03:37 -0800

    cors added

--- server.js
+++ server.js
@@ -9,17 +9,39 @@ dotenv.config();
 connectDB(); // Optional if you use a database
 
 const app = express();
-const PORT = process.env.PORT || 4000;
 
 // Middleware
 app.use(cors()); // Enable CORS
+// Middleware to parse JSON bodies in requests
 app.use(express.json());
+
+// CORS configuration
+const corsOptions = {
+    origin: ['http://localhost:8081', "http://localhost:3000"],
+    credentials: true, // Allow credentials such as cookies or authorization headers
+};
+app.use(cors(corsOptions)); // Use CORS middleware with options
 
 // Routes
 app.use('/recipes', recipeRoutes);
 app.use('/users', userRoutes);
 
+// Default route for the health check
+app.get('/', (req, res) => {
+    res.status(200).json({ status: 'API is Running' });
+});
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+    console.error(err.stack); // Log the error stack
+    res.status(500).json({ message: 'Something went wrong!' });
+});
+
+const PORT = process.env.PORT || 4000;
 // Start the server
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
+
+// Export the app for Vercel
+export default app;
